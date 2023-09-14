ab17a569c4b716d3d14b207726792fb4b1709998
Abhilash-1206 <111057823+Abhilash-1206@users.noreply.github.com>
2023-09-14 11:55:44 +0530

    Create Save_as_File.py

---
+++ Implementation/Save_as_File.py
@@ -0,0 +1,16 @@
+def save_as_file(formats, df, path, filename):
+    if formats:
+        file_type = formats["type"].lower()
+        delimiter = formats.get("delimiter")
+        extension = formats.get("extension").lower()
+
+        if file_type == "excel":
+            df.to_excel(path + filename + "." + extension, sep=delimiter, index=False)
+
+        elif file_type == "parquet":
+            df.to_parquet(path + filename + "." + extension, index=False)
+
+        else:
+            df.to_csv(path + filename + "." + extension, sep=delimiter, index=False)
+    else:
+        df.to_csv(path + filename + ".csv", index=False)
